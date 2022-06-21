8c3df43ac117fd00901fdc89849905763de9076b
Ismaaa2 <ibismaa2@gmail.com>
2022-06-21 22:27:21 +0200

    creación del script

---
+++ script_module_delete.py
@@ -0,0 +1,11 @@
+from os import walk
+import shutil
+
+def ls(ruta = 'D:\Estudios'):
+    listaarchivos = []
+    for (a, b, archivos) in walk(ruta):
+        for c in b:
+            if(c == 'node_modules'):
+                shutil.rmtree('{}\{}'.format(a,c))
+                listaarchivos.extend(c)
+print(ls())
